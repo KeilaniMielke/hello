a90ab5604dbf58079f8e909628b486d5612dae05
keilani.mielke <kmielke3@huskers.unl.edu>
2023-08-24 09:53:30 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Keilani Mielke
+ *         KMielke3@huskers.unl.edu
+ * Date: 2023/08/24
+ *
+ * A simple hello world program in C but with my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Keilani Mielke\n");
+
+  return 0;
+}
